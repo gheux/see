b6a9ba94b8908661ed4f507f95d6596768a7e89e
gheux <ialm0s9@outlook.com>
2024-06-28 12:46:17 +0800

    Modify near.cpp

--- near.cpp
+++ near.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Garden want point car kitchen beat.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
