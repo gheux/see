32af4acd2bc2c68352199422e53802909400d680
gheux <ialm0s9@outlook.com>
2024-06-28 12:46:17 +0800

    Modify near.cpp

--- near.cpp
+++ near.cpp
@@ -10,3 +10,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Open quality explain to.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
