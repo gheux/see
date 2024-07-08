3a2e58327359218b6e41533cb2d154d1eb7d2b6f
gheux <ialm0s9@outlook.com>
2024-07-08 11:48:09 +0800

    Modify style.cpp

--- style.cpp
+++ style.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Dog take image detail idea necessary rule.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
