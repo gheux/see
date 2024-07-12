0cb3720200a0bb4c204174e25ed5b8093d03c230
gheux <ialm0s9@outlook.com>
2024-07-12 11:54:27 +0800

    Modify style.cpp

--- style.cpp
+++ style.cpp
@@ -10,3 +10,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# House leader toward help year let.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
