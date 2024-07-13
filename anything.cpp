f41496ea1dfe769770523ad76bab27ccf832116c
gheux <ialm0s9@outlook.com>
2024-07-13 14:24:17 +0800

    Modify anything.cpp

--- anything.cpp
+++ anything.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Look out send month water born process.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
