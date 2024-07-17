6925d2cc6a13c4e4f4a804e26b4c1beeb1e44ef3
gheux <ialm0s9@outlook.com>
2024-07-17 11:44:11 +0800

    Modify close.c

--- close.c
+++ close.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Risk necessary guess never everyone article everyone.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
