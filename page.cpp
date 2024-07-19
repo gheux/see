c33c94292fd530b75c2d28dbcef01f7227dd70d4
gheux <ialm0s9@outlook.com>
2024-07-19 12:01:04 +0800

    Modify page.cpp

--- page.cpp
+++ page.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Practice wife ahead rule human.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
