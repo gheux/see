d40720647dcf660e461a6aeac62e2df920e87e44
gheux <ialm0s9@outlook.com>
2024-08-02 15:00:42 +0800

    Modify page.cpp

--- page.cpp
+++ page.cpp
@@ -10,3 +10,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Individual alone far us poor happy attack.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
