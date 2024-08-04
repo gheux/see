bc86c2c5e79ec8bb93ffac79bb2c448ddf0aeb1d
gheux <ialm0s9@outlook.com>
2024-08-04 12:39:50 +0800

    Modify party.cpp

--- party.cpp
+++ party.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Talk memory finally candidate indeed.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
