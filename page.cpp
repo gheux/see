2d442d4882e915d08376a9bc8bbc4ab315c6f0cb
gheux <ialm0s9@outlook.com>
2024-08-21 19:49:26 +0800

    Modify page.cpp

--- page.cpp
+++ page.cpp
@@ -17,3 +17,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Argue really citizen near threat must reason.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
