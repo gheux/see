5e56f242961a6431e94ce9470b79c07ae70065ff
gheux <ialm0s9@outlook.com>
2024-09-04 13:29:45 +0800

    Modify drug.go

--- drug.go
+++ drug.go
@@ -3,3 +3,10 @@ import "fmt"
 func main() {
 	fmt.Println("Hello, World!")
 }
+
+# Participant money hear few shake they plan.
+package main
+import "fmt"
+func main() {
+	fmt.Println("Hello, World!")
+}
