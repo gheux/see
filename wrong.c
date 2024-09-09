1e2ca08422683be37f9a1ca42348d5d9a7ab1443
gheux <ialm0s9@outlook.com>
2024-09-09 21:06:56 +0800

    Modify wrong.c

--- wrong.c
+++ wrong.c
@@ -17,3 +17,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Laugh argue road feel according require evidence.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
