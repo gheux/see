45a57150a53d58378758052db90fb1335d9d19ee
gheux <ialm0s9@outlook.com>
2024-09-20 19:42:34 +0800

    Modify the.cpp

--- the.cpp
+++ the.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Race response green kind.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
