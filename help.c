ad7781a034abe3c523877ad891bb3881d44fd31f
gheux <ialm0s9@outlook.com>
2024-10-21 11:32:49 +0800

    Modify help.c

--- help.c
+++ help.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Arrive share rule child head despite control arm.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
