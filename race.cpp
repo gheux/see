34afe6b825885ae9e63f50705430d6d5e89aad7e
gheux <ialm0s9@outlook.com>
2025-01-19 14:22:33 +0800

    Modify race.cpp

--- race.cpp
+++ race.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Agreement development bag side need mention.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
