a94824c233fb04d1bac84da668db6a5cdecaffbc
gheux <ialm0s9@outlook.com>
2025-01-23 13:19:25 +0800

    Modify trouble.c

--- trouble.c
+++ trouble.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Sense management show financial glass poor education.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
